a990e3e73ae44cf5d0958657cbe740650ac86a1b
Kuba Szczodrzyński <kuba@szczodrzynski.pl>
2025-02-17 17:10:47 +0100

    Add libfdt

--- lib/lib.mk
+++ lib/lib.mk
@@ -1,7 +1,8 @@
 LIB := lib
 FS_FAT := $(LIB)/fatfs
+LIBFDT := $(LIB)/libfdt
 
-CFLAGS += -I $(FS_FAT) -I $(LIB)
+CFLAGS += -I $(FS_FAT) -I $(LIBFDT) -I $(LIB)
 
 USE_FAT = $(shell grep -E "^\#define CONFIG_BOOT_(SDCARD|MMC)" boards/$(BOARD)/board.h)
 
@@ -11,6 +12,17 @@ SRCS    +=  $(FS_FAT)/diskio.c
 SRCS    +=  $(FS_FAT)/ffsystem.c
 SRCS    +=  $(FS_FAT)/ffunicode.c
 endif
+
+SRCS    +=  $(LIBFDT)/fdt_addresses.c
+SRCS    +=  $(LIBFDT)/fdt_check.c
+SRCS    +=  $(LIBFDT)/fdt_empty_tree.c
+SRCS    +=  $(LIBFDT)/fdt_overlay.c
+SRCS    +=  $(LIBFDT)/fdt_ro.c
+SRCS    +=  $(LIBFDT)/fdt_rw.c
+SRCS    +=  $(LIBFDT)/fdt_strerror.c
+SRCS    +=  $(LIBFDT)/fdt_sw.c
+SRCS    +=  $(LIBFDT)/fdt_wip.c
+SRCS    +=  $(LIBFDT)/fdt.c
 
 SRCS	+=  $(LIB)/atag.c
 SRCS	+=  $(LIB)/bootimg.c
